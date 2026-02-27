4389dadf20d24f02d2edee90b966b0976fdcf2ae
Milanya <yakovlevamilanya@yandex.ru>
2026-02-28 02:57:59 +0300

    json_file

--- Cheqmate/src/main/java/project/cheqmate/Application.java
+++ Cheqmate/src/main/java/project/cheqmate/Application.java
@@ -1,20 +1,28 @@
 package project.cheqmate;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Application {
 
     private final Scanner scanner;
     List<User> users; // можно сделать мапу <имя чела, User>, чтоб быстро по имени можно было найти
     List<Group> groups; // аналогично
+    ObjectMapper objectMapper;
+    private File user_file;
 
     Application(){
         scanner = new Scanner(System.in);
         users = new ArrayList<>();
         groups = new ArrayList<>();
+        objectMapper = new ObjectMapper();
     }
 
-    public void run() {
+    public void run() throws IOException {
 
         boolean running = true;
 
@@ -27,6 +35,7 @@ public class Application {
             System.out.println("4. Write the debts.");
             System.out.println("5. Print users and groups.");
             System.out.println("6. Exit.");
+            System.out.println("7. Make json");
             System.out.println("-----------------------------------------------------------------------------------------------------------------------------");
 
             int instructionNumber = scanner.nextInt();
@@ -49,7 +58,11 @@ public class Application {
                     printAll();
                     break;
                 case 6:
+                    make_json();
                     running = false;
+                    break;
+                case 7:
+                    make_json();
                     break;
                 default:
                     System.out.println("Unknown command.");
@@ -59,7 +72,7 @@ public class Application {
         scanner.close();
     }
 
-    public void createUser() {
+    public void createUser() throws IOException {
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------");
         System.out.println("Enter the username:");
 
@@ -71,7 +84,7 @@ public class Application {
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------");
     }
 
-    public void createGroup() {
+    public void createGroup() throws IOException{
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------");
         System.out.println("Enter the name of the group:");
 
@@ -90,15 +103,15 @@ public class Application {
 
         for (int i = 0; i < numberOfPeople; i++) {
             String name = scanner.nextLine();
-            User user = new User(name);
-            group.addMember(user);
+            add_info(name, "group", groupName);
+            group.addMember(findUserByName(name));
         }
 
         System.out.println("You created the group.");
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------");
     }
 
-    public void addCheque() {
+    public void addCheque() throws IOException{
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------");
         System.out.println("Add this cheque to which group? Type the group's name:");
 
@@ -111,10 +124,12 @@ public class Application {
         System.out.println("Enter the owner of the cheque:");
         String chequeOwnerString = scanner.nextLine();
         User chequeOwner = findUserByName(chequeOwnerString); // мб тут будет null
+        add_info(chequeOwnerString, "cheque_owner", chequeName);
 
         System.out.println("Enter the user who paid:");
         String whoPaidString = scanner.nextLine();
         User whoPaid = findUserByName(whoPaidString); // мб тут будет null
+        add_info(chequeOwnerString, "cheque_paid", chequeName);
 
         System.out.println("What is the cost of the cheque?");
         double total = scanner.nextDouble();
@@ -129,9 +144,12 @@ public class Application {
 
         System.out.println("Enter the " + Integer.toString(numberOfPeople) + " names of the users and how many percent of the total amount they owe:");
         for (int i = 0; i < numberOfPeople; i++) {
-            User user = findUserByName(scanner.next());
+            String name = scanner.next();
+            User user = findUserByName(name);
             double value = scanner.nextDouble();
             scanner.nextLine(); // пропуск /n
+            String info = "name of cheque: " + chequeName + ", debt: " + Double.toString(value * total / 100) + ", paid by: " + whoPaidString;
+            add_info(name, "cheque", info);
             cheque.addUser(user, value);
         }
 
@@ -139,24 +157,28 @@ public class Application {
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------");
     }
 
-    public void writeDebts() {
+    public void writeDebts() throws IOException {
         for (User user: users) {
             System.out.println(user.getName() + "'s debtors:");
             for(Map.Entry<User, Double> entry : user.getDebtors().entrySet()) {
                 String nameDebtor = entry.getKey().getName();
                 double amount = entry.getValue();
                 System.out.println("    " + nameDebtor + " - " + Double.toString(amount));
+                String info = "name of debtor: " + nameDebtor + ", debt: " + Double.toString(amount);
+                add_info(user.getName(), "debtors", info);
             }
             System.out.println(user.getName() + "'s creditors:");
             for(Map.Entry<User, Double> entry : user.getCreditors().entrySet()) {
                 String nameCreditor = entry.getKey().getName();
                 double amount = entry.getValue();
                 System.out.println("    " + nameCreditor + " - " + Double.toString(amount));
+                String info = "name of creditor: " + nameCreditor + ", debt: " + Double.toString(amount);
+                add_info(user.getName(), "creditors", info);
             }
         }
     }
 
-    private User findUserByName(String targetString) {
+    private User findUserByName(String targetString) throws  IOException {
         User target = null;
         for (User user: users) {
             if (Objects.equals(user.getName(), targetString)) {
@@ -166,6 +188,8 @@ public class Application {
         if (target == null) {
             System.out.println("There is no user with that name.");
             // придумать что делать дальше
+            System.out.println("Please create user");
+            createUser();
         }
         return target;
     }
@@ -192,6 +216,27 @@ public class Application {
         System.out.println("Groups:");
         for (Group group: groups) {
             System.out.println(group.getGroupName());
+        }
+    }
+
+    private void make_json() throws IOException {
+        for (User us : users) {
+            File dir = new File("src/main/java/user_files/");
+            user_file = new File(dir, "file_" + us.getName() + ".json");
+            user_file.createNewFile();
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(user_file, us.getInfo());
+        }
+        System.out.println("You created json");
+    }
+
+    private void add_info(String name, String key, String info) throws IOException {
+        User user = findUserByName(name);
+        if (user.getInfo().containsKey(key)) {
+            user.getInfo().get(key).add(info);
+        } else {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(info);
+            user.getInfo().put(key, list);
         }
     }
 }
--- Cheqmate/src/main/java/project/cheqmate/User.java
+++ Cheqmate/src/main/java/project/cheqmate/User.java
@@ -1,16 +1,25 @@
 package project.cheqmate;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class User {
     private final String name;
     private final HashMap<User, Double> debtors;
     private final HashMap<User, Double> creditors;
+    private final LinkedHashMap<String, ArrayList<String>> info;
 
-    User(String name_) {
+    User(String name_) throws IOException {
         debtors = new HashMap<>();
         creditors = new HashMap<>();
         name = name_;
+        info = new LinkedHashMap<>();
+        ArrayList<String> names = new ArrayList<>();
+        names.add(name);
+        info.put("name", names);
     }
 
     public void addDebtors(User person, double amount) {
@@ -31,5 +40,9 @@ public class User {
 
     public HashMap<User, Double> getCreditors() {
         return creditors;
+    }
+
+    public LinkedHashMap<String, ArrayList<String>> getInfo() {
+        return info;
     }
 }
